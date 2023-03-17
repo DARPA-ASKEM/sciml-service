e6c2df5b23b412de338dea23570ccaed32179366
Five Grant <5@fivegrant.com>
2023-03-17 12:55:56 -0500

    Add basic deterministic functionality

--- src/Executor.jl
+++ src/Executor.jl
@@ -1,18 +1,50 @@
+"""
+Interface for relevant ASKEM simulation libraries    
+"""
 module Executor
 
-import Oxygen: serveparallel, serve, resetstate, @post, @get
-import HTTP.Exceptions: StatusError 
+import AlgebraicPetri: LabelledPetriNet
+import Catlab.CategoricalAlgebra: parse_json_acset
+import Oxygen: serveparallel, serve, resetstate, json, @post, @get
+import CSV: write 
+import HTTP: Request
+import HTTP.Exceptions: StatusError
 import JobSchedulers: scheduler_start, set_scheduler, submit!, job_query, result, Job
 
-function submit()
+include("./deterministic.jl"); import .Deterministic: forecast
+
+"""
+Schedule a sim run
+"""
+function start_run(func)
+    # TODO(five): Spawn remote workers and run jobs on them
+    # TODO(five): Handle Python so a probabilistic case can work
     sim_run = Job(
-        Task(()->"Hello World"),
+        @task(func);
         priority = 5
     )
     submit!(sim_run)
     sim_run.id
 end
 
+"""
+Transform request body into splattable dict for kwargs only functions    
+"""
+function get_args(req::Request)::Dict{Symbol,Any}
+    args = json(req, Dict{Symbol, Any})
+    if !haskey(args, :petri)
+        args[:petri] = parse_json_acset(LabelledPetriNet, args[:petri])
+    end
+    if !haskey(args, :tspan)
+        args[:tspan] = Tuple(args[:tspan])
+    end
+    args
+end
+
+
+"""
+Hydrates a given function with a job
+"""
 function fill_job(func)
     function job_injected_func(_, id::Int64)
         job = job_query(id)
@@ -23,6 +55,30 @@ function fill_job(func)
     end
 end
 
+"""
+Find sim run and request a job with the given args    
+"""
+function make_deterministic_run(req::Request, name::String)
+    valid_funcs = Dict("forecast"=>forecast)
+    if !haskey(valid_funcs, name)
+        return StatusError(404, "GET", "GET", "Function not found")
+    end
+    chosen_sim = valid_funcs[name]
+    args = get_args(req)
+    # TODO(five): Transform request into labelled array splatted in func
+    # TODO(five): Support more return types other than CSV
+    # TODO(five): Write to remove server
+    start_run(()->(
+        sol = chosen_sim(; args...);
+        io = IOBuffer();
+        write(io, sol);
+        String(take!(io))
+    ))
+end
+
+"""
+Retrieve results of a sim run    
+"""
 function retrieve_results(job)
     if job.state != :done
         return StatusError(400, "GET", "GET", "Tried to access incomplete sim run")
@@ -30,12 +86,18 @@ function retrieve_results(job)
     result(job)
 end
 
+"""
+Specify endpoint to function mappings
+"""
 function register!()
-    @post "/submit" submit
+    @post "/run/deterministic/{name}" make_deterministic_run
     @get "/status/{id}" fill_job((job)->job.state)
     @get "/result/{id}" fill_job(retrieve_results)
 end
 
+"""
+Load API endpoints and start listening for sim run jobs
+"""
 function run!()
     resetstate()
     register!()
--- src/deterministic.jl
+++ src/deterministic.jl
@@ -1,27 +1,45 @@
+"""
+Deterministic Simulation Interface
+
+EXPOSED:
+- forecast   
+
+NEEDED:
+- fit
+"""
 module Deterministic
 
 import AlgebraicPetri: LabelledPetriNet
-import ModelingToolkit: ODESystem, ODEProblem, remake, solve
+import DataFrames: DataFrame
+import DifferentialEquations: solve
+import ModelingToolkit: ODESystem, ODEProblem, remake
 import Symbolics: getname
 import SymbolicIndexingInterface: states, parameters
 
-# using AlgebraicPetri, Catlab, JSON, JSON3, JSONTables, CSV, DataFrames, Oxygen, HTTP
-# using Catlab.CategoricalAlgebra
-# using Catlab.CategoricalAlgebra.FinSets
-# using ModelingToolkit, OrdinaryDiffEq, DifferentialEquations
-# using OrderedCollections, NamedTupleTools
+export forecast
 
-function symbolize_args(incoming_values, sys_symbols)
-    # In this context, standard symbols will be called vars as to not confilct with `Symbolics` symbols
+"""
+Transform list of args into Symbolics variables     
+"""
+function symbolize_args(incoming_values, sys_vars)
     pairs = collect(incoming_values)
-    incoming_vars, values = Symbol(first.(pairs)), last.(pairs)
-    symbols_as_vars = getname.(sys_symbols)
-    vars_to_symbols = Dict(symbols_as_vars .=> sys_symbols)
-    Dict([vars_to_symbols[symbols_as_vars[findfirst(x -> x == symbol, symbols_as_vars)]] for symbol in incoming_vars] .=> values)
+    symbols, values = Symbol(first.(pairs)), last.(pairs)
+    vars_as_symbols = getname.(sys_vars)
+    symbols_to_vars = Dict(vars_as_symbols .=> sys_vars)
+    Dict(
+        [
+         symbols_to_vars[vars_as_symbols[findfirst(x -> x == symbol, vars_as_symbols)]] 
+         for symbol in symbols
+        ] .=> values
+    )
 end
 
-function solve(petri::LabelledPetriNet, params, initials, tspan=(0.0, 100.0))
+"""
+Simulate a scenario from a PetriNet    
+"""
+function forecast(; petri::LabelledPetriNet, params, initials, tspan=(0.0, 100.0))
     # Convert PetriNet to ODEProblem
+    # TODO(five): Break out conversion into separate function maybe?
     sys = ODESystem(petri)
     sts = states(sys)
     ps = parameters(sys)
@@ -34,10 +52,9 @@ function solve(petri::LabelledPetriNet, params, initials, tspan=(0.0, 100.0))
         p=symbolize_args(params, ps),
         tspan=tspan
     )
-    solve(prob)
+    sol = solve(prob)
+    DataFrame(sol)
 end
-
-export solve
 
 end # module Deterministic
 
