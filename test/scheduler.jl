66e9a857cbd920b7803779d32a7da8e0bb26d018
Five Grant <5@fivegrant.com>
2023-03-22 12:25:40 -0500

    Fix tests

--- test/scheduler.jl
+++
@@ -1,5 +0,0 @@
-import Test: @test
-
-# TODO(five): Add actual tests for scheduler API
-
-@test 1 == 1
--- test/sciml.jl
+++
@@ -1,5 +0,0 @@
-import Test: @test
-
-# TODO(five): Add actual tests for SciML operations
-
-@test 1 == 1
