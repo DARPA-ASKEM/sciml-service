a191a5318f57d1f6a88d4ce5ca2447019d05392a
Five Grant <5@fivegrant.com>
2023-03-22 11:37:57 -0500

    Add test scaffolding

---
+++ test/scheduler.jl
@@ -0,0 +1,5 @@
+import Test: @test
+
+# TODO(five): Add actual tests for scheduler API
+
+@test 1 == 1
---
+++ test/sciml.jl
@@ -0,0 +1,5 @@
+import Test: @test
+
+# TODO(five): Add actual tests for SciML operations
+
+@test 1 == 1
