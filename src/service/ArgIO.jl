0d69f438eece1dc12a35585968a0e9ee60278c23
Five Grant <5@fivegrant.com>
2023-06-11 21:05:12 -0500

    Revert to old method of doing timespan

--- src/service/ArgIO.jl
+++ src/service/ArgIO.jl
@@ -13,23 +13,6 @@ include("./AssetManager.jl"); import .AssetManager: fetch_dataset, fetch_model, 
 
 export prepare_input, prepare_output
 
-"""
-Change arbitrary timespan to the range 0, 1, ...n
-"""
-function normalize_tspan(tstart, tend, stepsize)
-    diff = tend - tstart
-    if diff % stepsize != 0
-        throw("Steps exceed the end of timespan!")
-    end
-    (0, floor(diff/stepsize))
-end
-
-"""
-Transform naive step into epoch
-"""
-function get_step(tstart, stepsize, step)::Int
-    stepsize*step + tstart
-end
 
 """
 Transform requests into arguments to be used by operation    
@@ -52,10 +35,6 @@ function prepare_input(args; context...)
     if in(:model_config_ids, keys(args))
         args[:models] = fetch_model.(map(string, args[:model_ids]))
     end
-    if in(:timespan, keys(args)) && !isa(args[:timespan], AbstractArray)
-        span = args[:timespan]
-        args[:timespan] = normalize_tspan(span["start_epoch"],span["end_epoch"],span["tstep_seconds"])
-    end
     args
 end
 
@@ -76,10 +55,6 @@ Optionally, the CSV is saved to TDS instead an the coreresponding ID is returned
 function prepare_output(dataframe::DataFrame; context...)
     stripped_names = names(dataframe) .=> (r -> replace(r, "(t)"=>"")).(names(dataframe))
     rename!(dataframe, stripped_names)
-    if !isa(context[:raw_args], AbstractArray)
-        scale_step(step) = get_step(context[:raw_args][:timespan]["start_epoch"], context[:raw_args][:timespan]["tstep_seconds"], step)
-        dataframe.timestamp = map(scale_step, dataframe.timestamp)
-    end
     if in("upload", keys(context[:raw_args][:extra])) && !context[:raw_args][:extra]["upload"]
         io = IOBuffer()
         # TODO(five): Write to remote server
