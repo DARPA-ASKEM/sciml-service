332bd22797b72f8e63afe82599be6ff9e98b2a5b
Josh Day <emailjoshday@gmail.com>
2023-07-11 09:48:26 -0400

    wip

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Simulation Service
-Simulation Service provides an interface and job runner for [ASKEM models](https://github.com/DARPA-ASKEM/Model-Representations). 
+Simulation Service provides an interface and job runner for [ASKEM models](https://github.com/DARPA-ASKEM/Model-Representations).
 
 See example payload at [./examples/request.json](./examples/request.json)
 
 ## Usage
 
-With docker compose: 
+With docker compose:
 ```
 docker compose --file docker/docker-compose.yml up --build
 ```
@@ -13,9 +13,40 @@ docker compose --file docker/docker-compose.yml up --build
 With Julia REPL:
 
 ```
-julia> using SimulationService
-julia> start!()
-julia> # output of REST API and Scheduler
-julia> stop!()
-julia> # you may safely leave the repl or rerun `start!`
+>> export ENABLE_TDS=false         # Disable backend Terrarium Data Service
+>> julia --project --threads=auto  # Multithreading required for server
+```
+
+```julia
+using SimulationService, HTTP, JSON3
+
+# Start the server/job scheduler
+start!()
+
+# Get where server is running ("http:/127.0.0.1:8000" by default)
+url = SimulationService.server_url[]
+
+# JSON in ASKEM Model Representation
+amr = JSON3.read(HTTP.get("./examples/BIOMD0000000955_askenet.json").body)
+
+# Kick off the simulation job
+res = HTTP.post("$url/simulate", ["Content-Type" => "application/json"]; body=json)
+
+# Get the `job_id` so we can query the job status and get results
+job_id = JSON3.read(res.body).job_id
+
+# Re-run this until `status_obj.status == "complete"`
+status_obj = JSON3.read(HTTP.get("$url/status/$job_id").body)
+while status_obj.status != "complete"
+    status_obj = JSON3.read(HTTP.get("$url/status/$job_id").body)
+end
+
+# Get the result of the simulateion job
+result = JSON3.read(HTTP.get("$url/results/$job_id").body)
+
+# close down server and scheduler
+stop!()
+
 ````
+
+To check available endpoints, try checking [localhost:8080/docs](localhost:8080/docs)
--- src/SimulationService.jl
+++ src/SimulationService.jl
@@ -30,10 +30,11 @@ export start!, stop!
 
 # ASKEM Model Representation: https://github.com/DARPA-ASKEM/Model-Representations/blob/main/petrinet/petrinet_schema.json
 
-# pre-rewrite commit: https://github.com/DARPA-ASKEM/simulation-service/commit/8e4dfc515fd6ddf53067fa535e37450daf1cd63c
+# pre-rewrite commit: https://github.com/DARPA-ASKEM/simulation-service/tree/8e4dfc515fd6ddf53067fa535e37450daf1cd63c
 
 #-----------------------------------------------------------------------------# __init__
 const rabbit_mq_channel = Ref{Any}() # TODO: replace Any with what AMQPClient.channel returns
+const server_url = Ref{String}()
 
 function __init__()
     if Threads.nthreads() == 1
@@ -50,18 +51,23 @@ function __init__()
         conn = AMQPClient.connection(; virtualhost="/", host="localhost", port=SIMSERVICE_RABBITMQ_PORT, auth_params)
         rabbitmq_channel[] = AMQPClient.channel(conn, AMQPClient.UNUSED_CHANNEL, true)
     end
+
+    server_url[] = "http://$SIMSERVICE_HOST:$SIMSERVICE_PORT"
 end
 
 #-----------------------------------------------------------------------------# start!
 function start!(; host=SIMSERVICE_HOST, port=SIMSERVICE_PORT, kw...)
     Threads.nthreads() > 1 || error("Server require `Thread.nthreads() > 1`.  Start Julia via `julia --threads=auto`.")
     stop!()  # Stop server if it's already running
+    server_url[] = "http://$host:$port"
     JobSchedulers.scheduler_start()
     JobSchedulers.set_scheduler(max_cpu=0.5, max_mem=0.5, update_second=0.05, max_job=5000)
     Oxygen.resetstate()
     Oxygen.@get "/" health
-    Oxygen.@get "/status/{job_id}" (req,job_id) -> "TODO: Not Implemented yet."
-    Oxygen.@post "/{op}" operation
+    Oxygen.@post "/{operation_name}" operation
+    Oxygen.@get "/jobs/status/{job_id}" job_status
+    Oxygen.@get "/jobs/result/{job_id}" job_result
+    Oxygen.@post "/jobs/kill/{job_id}" job_kill
     Oxygen.serveparallel(; host, port, async=true, kw...)
 end
 
@@ -86,7 +92,7 @@ SIMSERVICE_RABBITMQ_PORT        = parse(Int, get(ENV, "SIMSERVICE_RABBITMQ_PORT"
 # Terrarium Data Service (TDS)
 SIMSERVICE_ENABLE_TDS           = get(ENV, "SIMSERVICE_ENABLE_TDS", "true") == "true"
 SIMSERVICE_TDS_URL              = get(ENV, "SIMSERVICE_TDS_URL", "http://localhost:8001")
-SIMSERVICE_TDS_UPDATE_RETRIES   = parse(Int, get(ENV, "SIMSERVICE_TDS_RETRIES", "10"))
+SIMSERVICE_TDS_RETRIES   = parse(Int, get(ENV, "SIMSERVICE_TDS_RETRIES", "10"))
 
 
 #-----------------------------------------------------------------------------# utils
@@ -135,42 +141,28 @@ function ode_system_from_amr(obj::JSON3.Object)
     ODESystem(eqs, t, statefuncs, paramvars; defaults = [statefuncs .=> initial_vals; sym_defs], name=Symbol(obj.name))
 end
 
-#--------------------------------------------------------------------------# Terrarium Data Service
-# DataFrame saved as CSV
-# Everything else saved as JSON
-function tds_upload(x, job_id::String; name::String="result")
-    if !SIMSERVICE_ENABLE_TDS
-        return @warn "TDS is not enabled.  `x::$(typeof(x))` will not be uploaded.))"
-    end
-    ext = x isa DataFrame ? "csv" : "json"
-    body = x isa DataFrame ? CSV.write(x) : JSON3.write(x)
-    upload_url = "$SIMSERVICE_TDS_URL/simulations/sciml-$job_id/upload-url?filename=$name.$ext"
-    url = get_json(upload_url)
-    HTTP.put(url, JSON_HEADER; body)
-    return first(split(url, '?'))
-end
-
-function tds_update_job(job_id::String, updated_fields::Dict{Symbol})
-    if !SIMSERVICE_ENABLE_TDS
-        return @warn "TDS is not enabled.  Job $job_id will not be updated: $update_fields."
-    end
-    obj = nothing
-    url = "$SIMSERVICE_TDS_URL/simulations/$job_id"  # joshday: rename simulations => jobs?
-    for _ in 1:SIMSERVICE_TDS_UPDATE_RETRIES
-        try
-            obj = get_json(url)
-        catch ex
-            ex isa HTTP.Exceptions.StatusError && ex.status == 404 ? continue : rethrow(ex)
-        end
-    end
-    isnothing(obj) && error("Cannot update. Job $job_id does not exist in TDS.")
-
-    body = JSON3.write(merge(Dict(obj), updated_fields))
-    HTTP.put(url, JSON_HEADER; body=body)
-end
-
 #-----------------------------------------------------------------------------# health: "GET /"
 health(::HTTP.Request) = (; status="ok", SIMSERVICE_RABBITMQ_ENABLED, SIMSERVICE_RABBITMQ_ROUTE)
+
+function job_status(::HTTP.Request, job_id::String)
+    job = JobSchedulers.job_query(job_id)
+    return job.state
+end
+
+function job_result(::HTTP.Request, job_id::String)
+    job = JobSchedulers.job_query(job_id)
+    JobSchedulers.result(job)
+end
+
+function job_kill(::HTTP.Request, job_id::String)
+    try
+        job = JobSchedulers.job_query(job_id)
+        JobSchedulers.cancel!(job)
+        return HTTP.Response(200)
+    catch
+        return HTTP.Response(500)
+    end
+end
 
 #-----------------------------------------------------------------------------# RabbitMQ
 # Content sent to client as JSON3.write(content)
@@ -183,28 +175,7 @@ function publish_to_rabbitmq(content)
 end
 publish_to_rabbitmq(; kw...) = publish_to_rabbitmq(Dict(kw...))
 
-#-----------------------------------------------------------------------------# IntermediateResults
-# Publish intermediate results to RabbitMQ with at least `every` seconds inbetween callbacks
-mutable struct IntermediateResults
-    last_callback::Dates.DateTime  # Track the last time the callback was called
-    every::Dates.TimePeriod  # Callback frequency e.g. `Dates.Second(5)`
-    job_id::String
 
-    function IntermediateResults(job_id::String; every=Dates.Second(5))
-        new(typemin(Dates.DateTime), every, job_id)
-    end
-end
-
-function (o::IntermediateResults)(integrator)
-    if o.last_callback + o.every ≤ Dates.now()
-        o.last_callback = Dates.now()
-        (; iter, t, u, uprev) = integrator
-        publish_to_rabbitmq(; iter=iter, time=t, params=u, abserr=norm(u - uprev), job_id=o.jobid,
-            retcode=SciMLBase.check_error(integrator))
-    end
-end
-
-get_callback(job_id::String) = DiscreteCallback((args...) -> true, IntermediateResults(job_id))
 
 
 #-----------------------------------------------------------------------------# Operations
@@ -231,15 +202,16 @@ abstract type Operation end
 
 #-----------------------------------------------------------------------------# OperationRequest
 # An OperationRequest contains all the information required to run an Operation
-
-struct OperationRequest
+# It's created immediately from a client request and we pass it around to keep all info together
+mutable struct OperationRequest{T <: Operation}
     obj::JSON3.Object                   # Untouched JSON from request body
     model_config_ids::Vector{UUIDs.UUID}
     amr::Vector{JSON3.Object}           # ASKEM Model Representations associated with model_config_ids
     timespan::Tuple{Float64, Float64}
     df::DataFrame                       # empty if !haskey(obj, :dataset)
     job_id::String                      # auto-generated in constructor
-    operation::String                   # e.g. "simulate"
+    operation_type::Type{T}
+    results::Any                        # results of solve(operation(::OperationRequest))
 
     function OperationRequest(req::HTTP.Request)
         obj = JSON3.read(req.body)
@@ -248,9 +220,11 @@ struct OperationRequest
         timespan = get_timespan(obj)
         df = get_df(obj)
         job_id = "sciml-$(UUIDs.uuid4())"
-        operation = split(req.url.path, ",")[end]
+        operation_name = split(req.url.path, ",")[end]
+        operation_dict = Dict(lowercase(string(T)) => T for T in subtypes(Operation))
+        operation_type = operation_dict[operation_name]
 
-        new(obj, model_config_ids, amr, timespan, df, job_id, operation)
+        new{operation_type}(obj, model_config_ids, amr, timespan, df, job_id, operation_type, nothing)
     end
 end
 
@@ -260,7 +234,7 @@ function get_model_config_ids(obj::JSON3.Object)
    elseif haskey(obj, :model_config_ids)
         return UUID.(obj.model_config_ids)
    else
-       error("JSON request to have a `model_config_id` or `model_config_ids` key.")
+       @warn "JSON request expected to have a `model_config_id` or `model_config_ids` key."
    end
 end
 
@@ -282,42 +256,105 @@ function get_df(obj::JSON3.Object)
     return df
 end
 
-#-----------------------------------------------------------------------------# POST /operation/{operation}
-function operation(req::HTTP.Request, op::String)
-    try
-        # Get T <: Operation from request
-        T = get_operation_type(op)
-        op_req = OperationRequest(req)
-        op = T(op_req)
-        callback = get_callback(op_req.job_id)
+#--------------------------------------------------------------------# IntermediateResults callback
+# Publish intermediate results to RabbitMQ with at least `every` seconds inbetween callbacks
+mutable struct IntermediateResults
+    last_callback::Dates.DateTime  # Track the last time the callback was called
+    every::Dates.TimePeriod  # Callback frequency e.g. `Dates.Second(5)`
+    job_id::String
+    function IntermediateResults(job_id::String; every=Dates.Second(5))
+        new(typemin(Dates.DateTime), every, job_id)
+    end
+end
+function (o::IntermediateResults)(integrator)
+    if o.last_callback + o.every ≤ Dates.now()
+        o.last_callback = Dates.now()
+        (; iter, t, u, uprev) = integrator
+        publish_to_rabbitmq(; iter=iter, time=t, params=u, abserr=norm(u - uprev), job_id=o.jobid,
+            retcode=SciMLBase.check_error(integrator))
+    end
+end
+get_callback(o::OperationRequest) = DiscreteCallback((args...) -> true, IntermediateResults(o.job_id))
 
-        # schedule job and update status when finished
-        tds_update_job(o.job_id, Dict(:status => "running", :start_time => time()))
-        t = @task function ()
-            output = solve(op; callback)
-            prepare_output(output, op_req)
-            tds_update_job(o.job_id, Dict(:status => "complete", :completed_time => time()))
+#--------------------------------------------------------------------------# Terrarium Data Service
+# Retry a function `n` times if error is HTTP 404
+default_retry_contion(ex) = ex isa HTTP.Exceptions.StatusError && ex.status == 404
+
+function retry_n(f; n::Int=SIMSERVICE_TDS_RETRIES, sleep_between::Int=1, condition=default_retry_contion)
+    res = nothing
+    for _ in 1:SIMSERVICE_TDS_UPDATE_RETRIES
+        try
+            res = f()
+            break
+        catch ex
+            if condition(ex)
+                sleep(sleep_between)
+                continue
+            else
+                rethrow(ex)
+            end
         end
-        sim_run = JobSchedulers.Job(t)
-        sim_run.id = o.job_id
-        JobSchedulers.submit!(sim_run)
+    end
+    return res
+end
 
+function update_job_status!(o::OperationRequest; kw...)
+    SIMSERVICE_ENABLE_TDS || return @info "update_job_status!: $kw"
+    job_id = o.job_id
+    url = "$SIMSERVICE_TDS_URL/simulations/$job_id"  # joshday: rename simulations => jobs?
+    obj = retry_n(() -> get_json(url))
+    # If JobID is not found in the TDS, we will create it
+    obj = isnothing(obj) ? Dict() : Dict(obj)
+
+    body = JSON3.write(merge(Dict(obj), Dict(kw)))
+    HTTP.put(url, JSON_HEADER; body=body)
+end
+
+
+function upload_results!(o::OperationRequest)
+    isnothing(o.results) && error("No results.  Run `solve!(o)` first.")
+
+    if o.results isa DataFrame
+        body = CSV.write(o.results)
+        filename = "result.csv"
+        header = ["Content-Type" => "text/csv"]
+    else
+        body = JSON3.write(o.results)
+        filename = "result.json"
+        header = JSON_HEADER
+    end
+
+    upload_url = "$SIMSERVICE_TDS_URL/simulations/sciml-$job_id/upload-url?filename=$filename"
+    url = get_json(upload_url).url
+    if SIMSERVICE_ENABLE_TDS
+        HTTP.put(url, header; body=body)
+    else
+        @info "upload_results!: $body"
+    end
+end
+
+#-----------------------------------------------------------------------------# solve!
+function solve!(o::OperationRequest)
+    update_job_status!(o; status="running", start_time=time())
+    operation = o.operation_type(o)
+    callback = get_callback(o)
+    o.results = solve(operation; callback)
+    upload_results!(o)
+    update_job_status!(o; status="complete", complete_time=time())
+    return o.result
+end
+
+#-----------------------------------------------------------------------------# POST /operation/{operation}
+function operation(req::HTTP.Request, operation_name::String)
+    try
+        o = OperationRequest(req)
+        job = JobSchedulers.Job(@task(solve!(o)); id=o.job_id)
+        JobSchedulers.submit!(job)
         body = JSON3.write((; simulation_id = o.job_id))
         return Response(201, ["Content-Type" => "application/json; charset=utf-8"]; body=body)
     catch ex
         return Response(500, ["Content-Type" => "application/json; charset=utf-8"]; body=JSON3.write((; error=string(ex))))
     end
-end
-
-function get_operation_type(op::String)
-    d = Dict(lowercase(string(T)) => T for T in subtypes(Operation))
-    return d[op]
-end
-
-# joshday: What else needs to be handled by prepare_output?
-function prepare_output(df::DataFrame, o::OperationRequest; name="0")
-    foreach(name -> rename!(df, name => replace(name, "(t)" => "")), names(df))
-    SIMSERVICE_ENABLE_TDS ? tds_upload(df, o.job_id; name=name) : CSV.write(df)
 end
 
 
