af379959d2648b98001ec30cf99bbdb6fd1b1f00
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-07-14 14:30:03 -0400

    Update SimulationService.jl

--- src/SimulationService.jl
+++ src/SimulationService.jl
@@ -2,7 +2,7 @@ module SimulationService
 
 import AMQPClient
 import CSV
-import DataFrames: DataFrame
+import DataFrames: DataFrame, rename!
 import Dates: Dates, DateTime, now, UTC
 import DifferentialEquations
 import Downloads: download
