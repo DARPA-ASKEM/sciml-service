8ae64379a91de713e3da16e4fb20dcc052c40db0
jClugstor <jadonclugston@gmail.com>
2023-12-04 12:06:49 -0500

    placeholder test for calibrate-ensemble route

--- test/runtests.jl
+++ test/runtests.jl
@@ -52,13 +52,13 @@ simulate_ensemble_payloads = JSON3.write.([
 
 calibrate_ensemble_payloads = JSON3.write.([(
             model_configs = map(1:2) do i
-                (id="model_config_id_$i", weight = i / sum(1:2), solution_mappings = (any_generic = "I", name = "R", s = "S"))
+                (id="model_config_id_$i", weight = 0.5, solution_mappings = (any_generic = "I", name = "R", s = "S"))
             end,
-            model_file_urls = ["https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/raw_models/SEIRD_base_model01.json",
-            "https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/raw_models/SEIRD_base_model01.json"],
+            model_file_urls = ["https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json",
+            "https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json"],
             timespan = (start = 0, var"end" = 40),
             engine = "sciml",
-            dataset_url = "https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/datasets/ensemble.csv",
+            dataset_url = "https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/datasets/SIDARTHE_dataset.csv",
             extra = (; num_samples = 40))]
         )
 
