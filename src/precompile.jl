4f82c10756f30f55640c8f94ca2b9f7228b9f25d
jClugstor <jadonclugston@gmail.com>
2023-12-06 10:37:55 -0500

    change precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,3 @@
 @compile_workload begin
-    SimulationService.amr_get(JSON3.read(joinpath(@__DIR__, "..", "examples", "calibrate_example1", "BIOMD0000000955_askenet.json")), ODESystem)
+    SimulationService.amr_get(JSON3.read(HTTP.get("https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json").body), ODESystem)
 end
