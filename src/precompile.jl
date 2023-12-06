f1074a06deaa4d73b6e88fa83b3660fc2cf1d7b2
jClugstor <jadonclugston@gmail.com>
2023-12-06 11:02:50 -0500

    add configuration

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,3 @@
 @compile_workload begin
-    SimulationService.amr_get(JSON3.read(HTTP.get("https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json").body), ODESystem)
+    SimulationService.amr_get(JSON3.read(HTTP.get("https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json").body).configuration, ODESystem)
 end
