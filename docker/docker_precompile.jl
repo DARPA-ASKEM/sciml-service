95a523271044dbe065aa68d6d492b3004397f2e5
jClugstor <jadonclugston@gmail.com>
2024-01-25 09:19:32 -0500

    Update Julia version in docker file

--- docker/docker_precompile.jl
+++ docker/docker_precompile.jl
@@ -1,5 +1,1 @@
 using SimulationService
-import ModelingToolkit: ODESystem
-import JSON3
-import HTTP
-SimulationService.amr_get(JSON3.read(HTTP.get("https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json").body).configuration, ODESystem)
