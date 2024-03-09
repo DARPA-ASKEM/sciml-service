a9f16a3af2ff5850a1558353447bb5a76bdf69f4
Yohann Paris <github@yohannparis.com>
2024-03-09 10:25:28 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -486,15 +486,14 @@ end
         end
 
         @testset "/model-equation" begin
-            for body in simulate_ensemble_payloads
-                res = HTTP.post("$url/model-equation", ["Content-Type" => "application/json"]; body)
-                @test res.status == 200
-                response_data = JSON3.read(res.body)
-                @test haskey(response_data, :latex)
-                latex = response_data[:latex]
-                @test typeof(latex) == String
-                # We could check if the response contains the expected LaTeX representation of the models
-            end
+            amr = JSON3.read(HTTP.get("https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/sidarthe.json").body)
+            response = HTTP.post("$url/model-equation", ["Content-Type" => "application/json"]; amr)
+            @test response.status == 200
+            response_data = JSON3.read(response.body)
+            @test haskey(response_data, :latex)
+            latex = response_data[:latex]
+            @test typeof(latex) == String
+            # We could check if the response contains the expected LaTeX representation of the models
         end
     end
 end
