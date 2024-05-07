46f48a07a238feeb8818e289648909a9ab77ebd5
jClugstor <jadonclugston@gmail.com>
2024-05-07 09:58:23 -0400

    rudimentary progress reporting

--- src/operations.jl
+++ src/operations.jl
@@ -184,18 +184,6 @@ mutable struct IntermediateResults
     end
 end
 
-function (o::IntermediateResults)(integrator)
-    (; iter, f, t, u, p) = integrator
-    if o.last_callback + o.every == iter
-        o.last_callback = iter
-        state_dict = Dict(states(f.sys) .=> u)
-        param_dict = Dict(parameters(f.sys) .=> p)
-        publish_to_rabbitmq(; iter=iter, state=state_dict, params = param_dict, id=o.id,
-            retcode=SciMLBase.check_error(integrator))
-    end
-    EasyModelAnalysis.DifferentialEquations.u_modified!(integrator, false)
-end
-
 # Intermediate results functor for calibrate
 function (o::IntermediateResults)(p,lossval, ode_sol, ts)
     if o.last_callback + o.every == o.iter
@@ -236,8 +224,22 @@ end
 
 # callback for Simulate requests
 function solve(op::Simulate; callback)
+
+    function (o::IntermediateResults)(integrator)
+        (; iter, f, t, u, p) = integrator
+        percent = round((t/op.timespan[2])*100.0, digits = 2)
+        if o.last_callback + o.every == iter
+            o.last_callback = iter
+            #state_dict = Dict(states(f.sys) .=> u)
+            #param_dict = Dict(parameters(f.sys) .=> p)
+            publish_to_rabbitmq(;id=o.id,
+                retcode=SciMLBase.check_error(integrator), percent = percent)
+        end
+        EasyModelAnalysis.DifferentialEquations.u_modified!(integrator, false)
+    end
+
     prob = ODEProblem(op.sys, [], op.timespan)
-    sol = solve(prob; progress = true, progress_steps = 1, saveat=1, callback = nothing)
+    sol = solve(prob; saveat=1, callback = nothing)
     @info "Timesteps returned are: $(sol.t)"
     dataframe_with_observables(sol)
 end
